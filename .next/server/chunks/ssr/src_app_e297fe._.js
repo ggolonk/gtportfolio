640e6b20e1340a2df4866382ed5fa40745a0b01f
ggolonk <gtgolonka91602@gmail.com>
2025-03-14 14:25:50 -0400

    ku

--- .next/server/chunks/ssr/src_app_e297fe._.js
+++ .next/server/chunks/ssr/src_app_e297fe._.js
@@ -581,8 +581,7 @@ function Projects() {
                                                     /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("img", {
                                                         src: selectedPicture,
                                                         alt: classCode,
-                                                        width: 400,
-                                                        height: 300
+                                                        width: 400
                                                     }, void 0, false, {
                                                         fileName: "[project]/src/app/projects/page.js",
                                                         lineNumber: 187,
--- .next/static/chunks/src_app_755e99._.js
+++ .next/static/chunks/src_app_755e99._.js
@@ -600,8 +600,7 @@ function Projects() {
                                                     /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("img", {
                                                         src: selectedPicture,
                                                         alt: classCode,
-                                                        width: 400,
-                                                        height: 300
+                                                        width: 400
                                                     }, void 0, false, {
                                                         fileName: "[project]/src/app/projects/page.js",
                                                         lineNumber: 187,
