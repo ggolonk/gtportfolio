0742123ce757d56ac07574d1b0fc92b934f1bbea
ggolonk <gtgolonka91602@gmail.com>
2025-11-11 15:56:58 -0500

    h

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,6 +24,7 @@ export default function RootLayout({ children }) {
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <NavBar />
         {children}
+        <Analytics/>
       </body>
     </html>
   );
