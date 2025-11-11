eed4cbb2a557063d8e70e5d442ce6d5d1e549539
ggolonk <gtgolonka91602@gmail.com>
2025-11-11 15:35:36 -0500

    hello

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/NavBar";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
